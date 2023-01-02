833cf021aee0a2a3f4ebc8bc0389db70194eb65a
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-01-02 23:31:49 +0300

    Added form

--- balance/models.py
+++ balance/models.py
@@ -5,8 +5,11 @@ PAYMENT_TYPE_OUT = 'out'
 
 # Create your models here.
 class Payment(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
     amount = models.FloatField()
     type = models.CharField(max_length=3, choices=(
         (PAYMENT_TYPE_IN, 'Поступление'),
         (PAYMENT_TYPE_OUT, 'Расход')
     ))
+    class Meta:
+        ordering = ('-created',)
--- balance/views.py
+++ balance/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
-
+from balance.models import Payment, PAYMENT_TYPE_IN
 # Create your views here.
 def payments_list(request):
-    return render(request, 'balance/list.html')
+    if request.method == 'POST':
+        data = request.POST
+        Payment.objects.create(amount=data['amount'], type=data['type'])
+    payments = Payment.objects.all()
+    balance = 0
+    for payment in payments:
+        if payment.type == PAYMENT_TYPE_IN:
+            balance += payment.amount
+        else:
+            balance -= payment.amount
+    return render(request, 'balance/list.html', {'payments':payments, 'balance': balance})
