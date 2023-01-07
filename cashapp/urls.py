8295badd990bb4ef0ff530b35c0786518be2da6e
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-01-07 15:22:02 +0300

    Fixed bugs for registration folder inside accounts django app!

--- cashapp/urls.py
+++ cashapp/urls.py
@@ -21,6 +21,6 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('balance.urls')),
-    path('accounts/', include('django.contrib.auth.urls')),
-
+    # path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/', include('accounts.urls'))
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
