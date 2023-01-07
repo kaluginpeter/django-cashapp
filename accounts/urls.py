032a19c9085f88c0bb08df4900b1becb41af7e99
paqstd <zebartcoc@gmail.com>
2023-01-07 16:30:54 +0300

    Added some bootstrap styles for auth templates

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,8 @@
 from django.contrib.auth import views
 from django.urls import path
-from accounts.forms import UserLoginForm
 
 urlpatterns = [
-    path("login/", views.LoginView.as_view(authentication_form=UserLoginForm), name="login"),
+    path("login/", views.LoginView.as_view(), name="login"),
     path("logout/", views.LogoutView.as_view(), name="logout"),
     path(
         "password_change/", views.PasswordChangeView.as_view(), name="password_change"
